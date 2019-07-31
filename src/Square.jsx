035fe62f55163e82454efaff22edfc1c8039f6ef
Andrew Ross <andrewross@Andrews-MacBook-Pro.local>
2019-07-31 18:43:36 +0200

    Render the value

--- src/Square.jsx
+++ src/Square.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Square extends React.Component {
     render() {
         return (<button className="square">
-
+            {this.props.value}
         </button>);
     }
 }
