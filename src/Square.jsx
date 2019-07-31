c71e62182c0934d746a4c4983f35c4167668e88d
Andrew Ross <andrewross@Andrews-MacBook-Pro.local>
2019-07-31 18:45:33 +0200

    Added state to square

--- src/Square.jsx
+++ src/Square.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
-        return (<button className="square">
-            {this.props.value}
+        return (<button
+            className="square"
+            onClick={() => this.setState({ value: 'X' })}
+        >
+            {this.state.value}
         </button>);
     }
 }
